06f2eb607e5411e5f1c7ab9ee4f1325055424197
decodewitness <vixenblue767@gmail.com>
2021-11-05 12:01:33 +0100

    fix env security.cpp

--- cfg/security.cpp
+++ cfg/security.cpp
@@ -7,16 +7,13 @@ int security_level = 1;
 std::string passcode = "ai";
 const std::string model_ai = "model-ai";
 
-
-
 void check_passcode(std::string pass) {
     //std::string environment_var = getenv(model_ai.c_str());
 
     if (/*environment_var.compare("setenv-aok") == false || */pass.compare(passcode) == false) {
         std::cout << "-:: ACCESS GRANTED" << std::endl;
-
-        //putenv("setenv-aok");
-
+        setenv("_MODEL_AI_AG", "Y", 1);
+        std::string x = getenv("_MODEL_AI_AG");
     } else {
         std::cout << "-:: ACCESS IS DENIED!" << std::endl;
         exit(0);
