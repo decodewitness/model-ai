374d4d6739d89ad7fe912b3b693d4fde37c54393
vera lo <vixenblue767@gmail.com>
2021-11-08 23:30:10 +0100

    commit headers fix name main.h etc

---
+++ main.h
@@ -0,0 +1,18 @@
+// HEADER.H - HEADER FILE FOR THE AI MODEL (USED BY MAIN.CPP)
+
+// C++ HEADERS
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <sys/types.h>
+#include <unistd.h>
+
+// CLASS LIBRARIES HEADERS
+#include "ai/ai.cpp"
+
+// INCLUDE SYSTEM
+
+// LOAD ARTIFICIAL LIFE ROUTINES
+//#include "al/al.cpp"
+
+// eof
