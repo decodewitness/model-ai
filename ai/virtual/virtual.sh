5bbcb50aa21447a53361d0261199a4a8b91bd571
vera lo <vixenblue767@gmail.com>
2021-11-09 21:45:32 +0100

    commit script run and AI

--- ai/virtual/virtual.sh
+++ ai/virtual/virtual.sh
@@ -1,7 +1,28 @@
-# NEED TO FIX THIS SCRIPT
-
 #!/bin/bash
 
-echo "executing \"virtual.sh\""
-echo "~you still need to create the virtual instance compressed archive and such."
+echo "-/- executing \"virtual.sh\"."
+
+
+
+pwd
+
+
+
+### fetch repositories
+echo "\t-/- fetching from repository."
+./ai/virtual/patch/repos.sh
+
+### prune files here ###
+
+# ./AI/model-ai.tar.gz
+# 
+#
+#
+#
+
+### run appliance ###
+echo "!!~you still need to create the virtual instance compressed archive and such."
+
+### end of script ###
 echo "done."
+exit 0
