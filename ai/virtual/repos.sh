9392e4adbbf780e795eec99a4f89ccc42cdf9ec4
vera lo <vixenblue767@gmail.com>
2021-11-10 04:38:12 +0100

    commit repository script update

--- ai/virtual/repos.sh
+++ ai/virtual/repos.sh
@@ -4,6 +4,8 @@
 LIST="./ai/patch/archives_list.csv"
 ARCHIVE1="./ai/patch/archive1.tar.gz"
 ARCHIVE2="./ai/patch/archive2.tar.gz"
+ARCHIVE2="./ai/patch/archive3.tar.gz"
+EXTRA="./ai/patch/extra.txt"
 
 ### URLS ###
 LIST_URL="https://model-ai.com/archives_list.csv"   # might contain additional archives
@@ -12,22 +14,21 @@ LIST_URL="https://model-ai.com/archives_list.csv"   # might contain additional a
 ARCHIVE_URL1="https://model-ai.com/archive1.tar.gz"
 ARCHIVE_URL2="https://model-ai.com/archive2.tar.gz"
 ARCHIVE_URL3="https://model-ai.com/archive3.tar.gz"
+EXTRA_URL="https://model-ai.com/extra.txt"
 
-### GET FILES DESCRIBED FROM REPOSITORY URLS ABOVE
+### GET ARCHIVE FILES LIST FETCHED FROM THE REPOSITORY URLS ABOVE AT HTTPS://MODEL-AI.COM
 echo "-/- getting \"archives_list.csv\""
 wget --output-file=$LIST $LIST_URL
 
 echo "-/- getting \"archives\" (.tar.gz format) from online repository"
-wget --output-file=$ARCHIVE $ARCHIVE_URL1
-wget --output-file=$ARCHIVE $ARCHIVE_URL2
-wget --output-file=$ARCHIVE $ARCHIVE_URL3
+wget --output-file=$ARCHIVE1 $ARCHIVE_URL1
+wget --output-file=$ARCHIVE2 $ARCHIVE_URL2
+wget --output-file=$ARCHIVE3 $ARCHIVE_URL3
 
 ### FETCH EXTRA DATA ###
+wget --output-file=$EXTRA $EXTRA_URL
 
-    # create for loop
-    #for [ i in 10 ]:
-
-
+### DONE WITH THE SCRIPT ###
 echo "done."
 exit 0
 
