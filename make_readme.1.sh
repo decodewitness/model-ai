e88f8d44fec98882dc334464bfc4057b0874d8fc
vera lo <vixenblue767@gmail.com>
2021-11-19 08:15:14 +0100

    commit scripts make_readme.sh and run

--- make_readme.1.sh
+++ make_readme.1.sh
@@ -13,14 +13,10 @@ fi
 if [ -d "./doc" ]
   then
     cp ./doc/README.md ./README.md.old
-    rm -r ./doc
-    mkdir -p ./doc
-    touch ./doc/README.md
-  else
+    rm -r ./doc/*
     mkdir -p ./doc
     touch ./doc/README.md
 fi
-
 ### download from archive the documents to constuct the "README.md" ###
 wget https://model-ai.com/archive/doc.tar.gz
 ### extract the "README.md" documents and clean up ###
@@ -29,47 +25,38 @@ if [ -f "./doc.tar.gz" ]
     tar xvf ./doc.tar.gz
     rm ./doc.tar.gz
 fi
-if [ -f "header1.txt" ]
-  then
-  mv ./header1.txt ./doc/
-fi
-if [ -f "header2.txt" ]
-  then
-  mv ./header2.txt ./doc
-fi
 if [ -f "./program.txt" ]
   then
-  mv ./program.txt ./doc/
+  mv ./program.txt ./doc
 fi
 if [ -f "./authors.txt" ]
   then
-  mv ./authors.txt ./doc/
+  mv ./authors.txt ./doc
 fi
 if [ -f "./copyrightlaw.txt" ]
   then
-  mv ./copyrightandlaw.txt ./doc/
+  mv ./copyrightandlaw.txt ./doc
 fi
 if [ -f "./readme_component_1.txt" ]
   then
-  mv ./readme_component_1.txt ./doc/
+  mv ./readme_component_1.txt ./doc
 fi
 if [ -f "./readme_component_2.txt" ]
   then
-  mv ./readme_component_2.txt ./doc/
+  mv ./readme_component_2.txt ./doc
 fi
 if [ -f "./make_readme.sh" ]
   then
-  mv ./make_readme.sh ./doc/
+  mv ./make_readme.sh ./doc
 fi
 if [ -f "./doc.tar.gz" ]
   then
-  mv ./doc.tar.gz ./doc/
+  mv ./doc.tar.gz ./doc
 fi
 ### create the document "README.md" inside the "/doc" directory ###
 if [ -f "./doc/program.txt" ]
   then
   cat ./doc/program.txt >> ./doc/README.md
-  rm ./doc/program.txt
 fi
 if [ -f "./doc/authors.txt" ]
   then
@@ -77,10 +64,9 @@ if [ -f "./doc/authors.txt" ]
   rm ./doc/authors.txt
   echo "//=====================================" >> README.md
 fi
-if [ -f "./doc/header1.txt" ]
+if [ -f ./doc/header1.txt ]
   then
   cat ./doc/header1.txt >> ./doc/README.md
-  rm ./doc/header1.txt
 fi
 if [ -f "./doc/beneficial.txt" ]
   then
@@ -115,7 +101,6 @@ fi
 if [ -f "./doc/header2.txt" ]
   then
   cat ./doc/header2.txt >> ./doc/README.md
-  rm ./doc/header2.txt
 fi
 if [ -f "./README.md.1" ]
   then
@@ -126,6 +111,7 @@ if [ -f "./doc/README.md" ]
   cp ./doc/README.md ./README.md
 fi
 ### do your clean up here ###
+
 
 ### exit the script ###
 exit 0
