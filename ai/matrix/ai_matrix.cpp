77a5033edea2c4c0178f257492f7def6dfc8e222
vera lo <vixenblue767@gmail.com>
2021-12-03 03:33:17 +0100

    commit matrix

--- ai/matrix/ai_matrix.cpp
+++ ai/matrix/ai_matrix.cpp
@@ -1,461 +1,103 @@
 #include "ai_matrix.h"
 
 AI_Matrix::AI_Matrix(int x, int y, int z) { // square measure (3, 3, 3)
-    // x=top y=middle z=bottom (section of square of 9) (max: 3)
-    bool square[10];
-    int a, b, c;
-
-    //     a    b    c
-    // 1 ixxx ixxx ixxx
-    // 2  xxx  xxx  xxx
-    // 3  xxx  xxx  xxx
-
-    int A_square[10]; // a,a1;
-    int B_square[10]; // a,b2;
-    int C_square[10]; // a,b3;
-    int decimal[30]; // 30 decimals
-   
-    switch (x) { // 120 A_square units {x+y+z=360}
+        // x=top y=middle z=bottom (section of square of 9) (max: 3)
         
-        case 0: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = -2;
-                break;
+        bool square[10];
+        int a, b, c;
 
-        case 1: A_square[0] = 1;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
+        //     a    b    c
+        // 1 ixxx ixxx ixxx
+        // 2  xxx  xxx  xxx
+        // 3  xxx  xxx  xxx
 
-        case 2: A_square[0] = 2;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
+
+        float square_A[10]; // a,a1;
+        float square_B[10]; // a,b2;
+        float square_C[10]; // a,b3;
+    
+        // decimal plane
+        float decimal_A[30]; // 30 decimals
+        float decimal_B[30]; // 30 decimals
+        float decimal_C[30]; // 30 decimals
+
+        float float_A[30];      // 30 floats
+        float float_B[30];      // 30 floats
+        float float_C[30];      // 30 floats
+
+
+// legend for the matrix a,b,c[0] = N_square[n]=x,y,z
+        /* 
+         * case n: A_square[0] = x;     // first number
+         *         A_square[1] = 0;     // second number
+         *         A_square[2] = 0;     // 3rd number or quotations
+         *         A_square[3] = 0;     // minus fractions
+         *         A_square[4] = 0;     // level of quotation
+         *         A_square[5] = 0;     // integral sum of all numbers
+         *         A_square[6] = 0;     // size of accumulated number fractions
+         *         A_square[7] = 0;     // length of decimals (nnnth)
+         *         A_square[8] = 0;     // planar scales
+         *         A_square[9] = 0;     // size of strings in quotation
+         * break;
+         *         B_square[0] = y;     // spreading values over B_square[array];
+         *         B_square[9] = x;
+         * 
+         *         decimalA[0] = B*(z/n*2);     // measures in plane
+         *         decimalA[9] = B*(z/n*2);
+         *
+         *         decimalB[9] = z;
+         *
+         *         decimalC[0] = z;
+         */
+
         
-        case 3: A_square[0] = 3;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
+        // alpha computations
+        square_A[0] = x;
+        square_A[1] = y;
+        square_A[2] = z;
 
-        case 4: A_square[0] = 4;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
+        // beta computations
+        square_B[0] = y;
+        square_B[9] = z;
 
-        case 5: A_square[0] = 5;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-                
-        case 6: A_square[0] = 6;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
+        // calculations
+        square_C[9] = x;
 
-        case 7: A_square[0] = 7;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-                
-        case 8: A_square[0] = 8;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
+        // draw decimal equations
+        decimal_A[0] = x+x;
+        decimal_A[1] = x*x;
+        decimal_A[2] = x/x;
+        decimal_A[3] = x*2;
+        decimal_A[4] = x/2;
+        decimal_A[5] = x/3;
+        decimal_A[6] = x/1.5;
+        decimal_A[7] = x/4;
+        decimal_A[8] = x/10;
+        decimal_A[9] = x/100;
 
-        case 9: A_square[0] = 9;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;     
-                break;
+        decimal_B[0] = y+y;
+        decimal_B[1] = y*y;
+        decimal_B[2] = y/y;
+        decimal_B[3] = y*2;
+        decimal_B[4] = y/2;
+        decimal_B[5] = y/3;
+        decimal_B[6] = y/1.5;
+        decimal_B[7] = y*2.5;
+        decimal_B[8] = y/10;
+        decimal_B[9] = y/100;
 
-        case 10:
-                A_square[0] = 10;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;     
-                break;
-
-        default:
-                A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 1;
-            break;
-    };
-
-    switch (y) {
-         case 0: A_square[0] = 0;
-                 A_square[1] = 1;
-                 A_square[2] = 0;
-                 A_square[3] = 0;
-                 A_square[4] = 0;
-                 A_square[5] = 0;
-                 A_square[6] = 0;
-                 A_square[7] = 0;
-                 A_square[8] = 0;
-                 A_square[9] = -2;
-                 break;
-         
-         case 1: A_square[0] = 0;
-                 A_square[1] = 1;
-                 A_square[2] = 0;
-                 A_square[3] = 0;
-                 A_square[4] = 0;
-                 A_square[5] = 0;
-                 A_square[6] = 0;
-                 A_square[7] = 0;
-                 A_square[8] = 0;
-                 A_square[9] = 0;
-                 break;
-
-        case 2: A_square[0] = 0;
-                A_square[1] = 2;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 3: A_square[0] = 0;
-                A_square[1] = 3;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-        
-        case 4: A_square[0] = 0;
-                A_square[1] = 4;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 5: A_square[0] = 0;
-                A_square[1] = 5;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 6: A_square[0] = 0;
-                A_square[1] = 6;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-                
-        case 7: A_square[0] = 0;
-                A_square[1] = 7;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 8: A_square[0] = 0;
-                A_square[1] = 8;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-                
-        case 9: A_square[0] = 0;
-                A_square[1] = 9;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 10:
-                A_square[0] = 0;
-                A_square[1] = 10;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;     
-                break;
-
-        default:
-                A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 1;
-            break;
-    };
-
-    switch (z) {
-        case 0: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = -2;
-                break;
-         
-        case 1: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 1;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 2: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 2;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 3: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 3;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-        
-        case 4: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 4;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 5: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 5;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 6: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 6;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-                
-        case 7: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 7;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 8: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 8;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-                
-        case 9: A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 9;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 0;
-                break;
-
-        case 10: A_square[0] = 0;
-                 A_square[1] = 0;
-                 A_square[2] = 10;
-                 A_square[3] = 0;
-                 A_square[4] = 0;
-                 A_square[5] = 0;
-                 A_square[6] = 0;
-                 A_square[7] = 0;
-                 A_square[8] = 0;
-                 A_square[9] = 0;     
-                 break;
-
-        default:
-                A_square[0] = 0;
-                A_square[1] = 0;
-                A_square[2] = 0;
-                A_square[3] = 0;
-                A_square[4] = 0;
-                A_square[5] = 0;
-                A_square[6] = 0;
-                A_square[7] = 0;
-                A_square[8] = 0;
-                A_square[9] = 1;
-            break;
-    };
+        decimal_C[0]  = x+y;
+        decimal_C[1]  = x-y;
+        decimal_C[2]  = x*y;
+        decimal_C[3]  = x/y;
+        decimal_C[4]  = y/x;
+        decimal_C[5]  = x + (x+y);
+        decimal_C[6]  = x + (x-y);
+        decimal_C[7]  = x + (x*y);
+        decimal_C[8]  = x + (x/y);
+        decimal_C[9]  = x + (y-x);
+        decimal_C[10] = x + (y/x);
+        decimal_C[11] = x + (y*2);
+        decimal_C[12] = x + (x*y*2);
+        decimal_C[13] = x + (y*2);
 };
