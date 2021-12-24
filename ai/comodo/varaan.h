ada7b3cff0753b537c4a46e8eeab422943183d8c
vera lo <vixenblue767@gmail.com>
2021-12-23 20:33:42 -0500

    commit _DEPLOY

--- ai/comodo/varaan.h
+++ ai/comodo/varaan.h
@@ -1,10 +1,10 @@
-const char TOOL[] = "ai/virtual/creation/tool";
 const char COMMANDOS[] = "ai/bin/commandos";
-const char _VARAAN[]   = "ai/bin/comodos";
 const char _COMODOS[]  = "ai/system/commandos/commandos.sh";
-const char _RESTOREMODEL[] = "ai/patching/restore";
-const char _PATCH[] = "ai/patching/patch";
-const char _TOOLDEPLOY[] = "ai/virtual/compile_tool";
+const char _VARAAN[]   = "ai/bin/comodos";
+const char _DEPLOY[] = "ai/virtual/creation/compile_tool";
 const char _INFILE[] = "ai/data/cab,ai/data/nop";
+const char _PATCH[] = "ai/patching/patch";
+const char _MODEL[] = "ai/patching/restore";
+const char _TOOL[] = "ai/bin/tool";
 
 // EOF
