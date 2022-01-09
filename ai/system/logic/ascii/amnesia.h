c76e28324cea335d6ea42fad17714ced5b81a5b4
vera lo <vixenblue767@gmail.com>
2022-01-09 15:20:01 +0100

    commit hashing mechanism

--- ai/system/logic/ascii/amnesia.h
+++ ai/system/logic/ascii/amnesia.h
@@ -24,8 +24,6 @@ int amnesia(int constr=55) {   // we have given our own representation to this c
   char b[1760];
   float A=0,B=0,i,j,z[1760];
 
-  printf("\x1b[2J");
-
   for(int ij=0;ij<constr;ij++){
     memset(b,32,1760);
     memset(z,0,7040);
@@ -35,7 +33,7 @@ int amnesia(int constr=55) {   // we have given our own representation to this c
         float c=sin(i),d=cos(j),e=sin(A),f=sin(j),g=cos(A),h=d+2,D=1/(c*h*e+f*g+5),l=cos(i),m=cos(B),n=sin(B),t=c*h*g-f*e;int x=40+30*D*(l*h*m-t*n),y=12+15*D*(l*h*n+t*m),o=x+80*y,N=8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n);if(22>y&&y>0&&x>0&&80>x&&D>z[o]){z[o]=D;;;b[o]=".,+~:;+1*248"[N>0?N:0];
       }
     }
-    printf("\x1b[H");for(k=0;1761>k;k++)
+    for(k=0;1761>k;k++)
     putchar(k%80?b[k]:10);A+=0.04;B+=0.02;
   }
 
