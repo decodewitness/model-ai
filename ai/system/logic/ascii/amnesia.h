14c654b2772df11ad6dfa55aaf2f3b3ab3b1372e
vera lo <vixenblue767@gmail.com>
2022-01-13 01:28:12 +0100

    commit amnesia fix

--- ai/system/logic/ascii/amnesia.h
+++ ai/system/logic/ascii/amnesia.h
@@ -28,13 +28,25 @@ int amnesia(int constr=55) {   // we have given our own representation to this c
     memset(b,32,1760);
     memset(z,0,7040);
   
-    for(j=0;6.28>j;j+=0.07)
+    for(j=0;6.28>j;j+=0.07) {
       for(i=0;6.28>i;i+=0.02){
-        float c=sin(i),d=cos(j),e=sin(A),f=sin(j),g=cos(A),h=d+2,D=1/(c*h*e+f*g+5),l=cos(i),m=cos(B),n=sin(B),t=c*h*g-f*e;int x=40+30*D*(l*h*m-t*n),y=12+15*D*(l*h*n+t*m),o=x+80*y,N=8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n);if(22>y&&y>0&&x>0&&80>x&&D>z[o]){z[o]=D;;;b[o]=".,+~:;+1*248"[N>0?N:0];
+        float c=sin(i),
+        d=cos(j), e=sin(A), f=sin(j),
+        g=cos(A),h=d+2,D=1/(c*h*e+f*g+5),
+        l=cos(i),m=cos(B),n=sin(B),
+        t=c*h*g-f*e;int x=40+30*D*(l*h*m-t*n),
+        y=12+15*D*(l*h*n+t*m),
+        o=x+80*y,N=8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n);
+        
+        if (22>y&&y>0&&x>0&&80>x&&D>z[o]){
+          z[o]=D;;;b[o]=".,+~:;+1*248"[N>0?N:0];
+        }
       }
     }
-    for(k=0;1761>k;k++)
-    putchar(k%80?b[k]:10);A+=0.04;B+=0.02;
+
+    for(k=0;1761>k;k++) {
+      putchar(k%80?b[k]:10);A+=0.04;B+=0.02;
+    }
   }
 
 return 1;
