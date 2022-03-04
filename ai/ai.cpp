e59d4a4c42f39b1668ad7d9db4e52761e08e4e33
vera lo <vixenblue767@gmail.com>
2022-03-05 00:00:01 +0100

    commit merge intelligence

--- ai/ai.cpp
+++ ai/ai.cpp
@@ -1482,7 +1482,7 @@ void AI::assembleBrain() {	// uses logic // useBrain && neural_net
 
 	// move this logic somewhere else
 	this->brain->useBrain(query_string);
-	//this->brain->neural_net(query_string);
+	this->brain->neural_net(query_string);
 
 	this->brn = true;
 };
