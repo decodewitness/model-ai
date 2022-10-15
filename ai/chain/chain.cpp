76c09a1fc54765f5d418b81890add1e9887fd755
vera lo <vixenblue767@gmail.com>
2022-10-14 20:44:14 -0400

    committing chains in chain.cpp

---
+++ ai/chain/chain.cpp
@@ -0,0 +1,21 @@
+// AI/CHAIN/CHAIN.CPP -- (NOT USED YET!!!)
+# include "chain.h"
+
+    // Need to use CHAIN.H to build chain dynamics in architecture.
+
+Chain & chainCreate() {
+    Chain *c = new Chain;
+    return *c;
+}
+
+void readChain(Chain c) {
+    // enumerate chain
+}
+
+void writeChain(int x) {
+    // write to chain
+}
+
+void writeChainData(char * str) {
+    
+}
---
+++ ai/chain/chain.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// length of chain "descriptor" or content.
+const int strlimit = 1024; // length of char sequence
+
+// utilize nr of chains // e.g. 5 (or 500, 47, etc.)
+const int maxchains = 5;
+
+// length of stored chain refs.
+const int limits = 5; // e.g. 10 (or 5, or 100) 
+
+// Main Chain structure.
+struct Chain {
+    int chainNr;    // id
+    int nrOfChains; // nr   // ((( could fluctuate ;; needs implementation)))
+    int activeChain;    // the active chain number (passive/active ##)
+    
+    // current nominal chain sequences
+        // from 1 -> ... -> 5
+    int sequence_one[limits];
+    int sequence_two[limits];
+    int sequence_three[limits];
+    int sequence_four[limits];
+    int sequence_five[limits];
+
+    // data or reference
+    int chain[maxchains][limits];
+    char chains[maxchains][strlimit];
+
+    // total pooled data
+    std::vector<std::string> pool;
+        // need to implement stringing mechanism to process this pool back and forth into stringed {} metadata.
+        // the pool fills up with data.
+        // string references are tied to definitions.
+        // pool empties in random order.
+        // sort of swap;
+};
